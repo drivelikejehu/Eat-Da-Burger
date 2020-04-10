98b4097f62fa6ad40be0bf14f193d3d36d56d95e
John Kang <pollyfactor@hotmail.com>
2020-04-09 21:20:11 -0400

    created burger_db in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,8 @@
-### Schema
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE TABLE burger (
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT FALS
 );
