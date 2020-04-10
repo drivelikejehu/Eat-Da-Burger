5585142af1a4b3f22bd8103d6791d45f9ca9419d
John Kang <pollyfactor@hotmail.com>
2020-04-09 20:45:25 -0400

    updated connction.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
