3e8fc1fe6f711bbd81159964556b894f226de6cc
John Kang <pollyfactor@hotmail.com>
2020-04-10 19:43:36 -0400

    updated orm.selectall

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-const connection = require("..config/connection.js");
+const connection = require("../config/connection.js");
 
 // * In the `orm.js` file, create the methods that will execute the necessary MySQL 
 //commands in the controllers. These are the methods you will need to use in order 
@@ -7,5 +7,17 @@ const connection = require("..config/connection.js");
 // * `selectAll()`
 // * `insertOne()`
 // * `updateOne()`
+
+const orm = {
+    selectAll: function(table, cb) {
+    var selectQuery = `SELECT * FROM ${table};`;
+    connection.query(selectQuery, function (err, res) {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+    })
+    }
+}
 
 module.exports = orm;
