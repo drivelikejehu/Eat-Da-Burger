fb7a0028caa1f352c6c189f4b798c5b44108a09e
John Kang <pollyfactor@hotmail.com>
2020-04-09 21:21:08 -0400

    updated values in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Bleu Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Double Cheese Butterburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Steamed Hams Hamburger', false);
