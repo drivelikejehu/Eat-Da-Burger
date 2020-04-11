b738347c8fdaf0df55bfb0052a1c5ed52444544a
John Kang <pollyfactor@hotmail.com>
2020-04-10 22:47:17 -0400

    updated schema with correct table name burgers

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burger (
+CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT FALSE,
