8ef847cf02608336941fcaea3d64ab0d4de7bcad
John Kang <pollyfactor@hotmail.com>
2020-04-10 21:41:58 -0400

    updated put route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,6 +23,21 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
