24b8918bc88d755ce3d71d31c89184172e46f166
John Kang <pollyfactor@hotmail.com>
2020-04-10 22:48:02 -0400

    updated seeds with correct table name

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Bleu Burger', false);
-INSERT INTO burger (burger_name, devoured) VALUES ('Double Cheese Butterburger', false);
-INSERT INTO burger (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', false);
-INSERT INTO burger (burger_name, devoured) VALUES ('Steamed Hams Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Bleu Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Butterburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steamed Hams Hamburger', false);
