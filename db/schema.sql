cb52941468f658607f2a81a81099c212f1c6a470
John Kang <pollyfactor@hotmail.com>
2020-04-11 16:39:31 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,8 @@ CREATE TABLE burgers (
 	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(id)
 );
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Bleu Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Butterburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steamed Hams Hamburger', false);
