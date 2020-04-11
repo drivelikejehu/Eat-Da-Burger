9edc9d47b584cd9f685233ae082be3569b2f15e0
John Kang <pollyfactor@hotmail.com>
2020-04-10 22:17:42 -0400

    changed devoured to false

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Bleu Burger', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Double Cheese Butterburger', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Bleu Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Double Cheese Butterburger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', false);
 INSERT INTO burger (burger_name, devoured) VALUES ('Steamed Hams Hamburger', false);
