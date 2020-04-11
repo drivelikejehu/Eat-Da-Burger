beef471ba965d29277a294537a0403d9f2c14490
John Kang <pollyfactor@hotmail.com>
2020-04-10 22:33:32 -0400

    refactored burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -39,17 +39,5 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.deleteOne(condition, function(result) {
-    if (result.affectedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
 // Export routes for server.js to use.
 module.exports = router;
