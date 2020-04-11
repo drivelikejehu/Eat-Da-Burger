6fab25dbfe698f3fe053e5cbc31c38ffddf1e378
John Kang <pollyfactor@hotmail.com>
2020-04-10 22:57:14 -0400

    updated burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -33,15 +33,4 @@ $(function() {
           location.reload();
         });
     });
-
-    $(".killburger").on("click", function(event) {
-        event.preventDefault();
-    
-        var id = $(this).data("id");
-
-        $.ajax({
-          type: "DELETE",
-          url: "/api/burgers/" + id
-        }).then(location.reload());
-      });
-    });
+});
