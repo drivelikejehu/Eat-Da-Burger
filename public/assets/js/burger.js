595fa7c9d0ae35cb458cc229345aa9503a3ed777
John Kang <pollyfactor@hotmail.com>
2020-04-10 22:15:47 -0400

    updated ajax calls

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,59 +1,47 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-sleep").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-  
-      var newSleepState = {
-        sleepy: newSleep
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
+$(function() {  
     $(".create-form").on("submit", function(event) {
       event.preventDefault();
   
       var newBurger = {
         burger_name: $("#newburger").val().trim(),
-        devoured: 0};
+        devoured: 0
+        };
   
-      // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
       }).then(
         function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
+          console.log("Order up!");
           location.reload();
-        }
-      );
+        });
     });
   
-    $(".delete-cat").on("click", function(event) {
+    $(".devour-burger").on("click", function(event) {
       var id = $(this).data("id");
+      var ifDevoured = {
+        devoured: 1
+      };
   
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: ifDevoured
       }).then(
         function() {
-          console.log("deleted cat", id);
-          // Reload the page to get the updated list
+          console.log("Burger consumed");
           location.reload();
-        }
-      );
+        });
     });
-  });
-  
+
+    $(".killburger").on("click", function(event) {
+        event.preventDefault();
+    
+        var id = $(this).data("id");
+
+        $.ajax({
+          type: "DELETE",
+          url: "/api/burgers/" + id
+        }).then(location.reload());
+      });
+    });
