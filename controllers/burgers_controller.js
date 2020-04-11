0cee2246ce2f355f192c8259e4bc779e7237a2e6
John Kang <pollyfactor@hotmail.com>
2020-04-10 21:38:45 -0400

    updated post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,17 +1,28 @@
 const express = require("express");
-const router = express.Router();
 const burger = require("../models/burger.js");
+const router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
-    var hbsObject = {
+    var handlebarsObj = {
       burgers: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    console.log(handlebarsObj);
+    res.render("index", handlebarsObj);
   });
 });
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
 
 // Export routes for server.js to use.
 module.exports = router;
