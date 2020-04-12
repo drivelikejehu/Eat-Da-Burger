c0f66800c0577b019b9715ff2d0624141083d5f5
John Kang <pollyfactor@hotmail.com>
2020-04-11 20:24:28 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,8 +8,3 @@ CREATE TABLE burgers (
 	devoured BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY(id)
 );
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Bleu Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Butterburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Steamed Hams Hamburger', false);
