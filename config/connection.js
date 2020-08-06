c48df43d525076c52c0a6f0f7852306c730c82b9
John Kang <pollyfactor@hotmail.com>
2020-08-06 14:52:44 -0400

    heroku changes

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 var mysql = require("mysql");
-var connection;
+// var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-connection = mysql.createConnection({
-host: "localhost",
-port: 3306,
-user: "root",
-password: "root1049",
-database: "burgers_db"
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root1049",
+    database: "burgers_db",
+  });
 }
 
 connection.connect();
